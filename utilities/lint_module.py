f21ab24f2e9ce0104d3eb66b176907cb274174b5
Avanteesh Motheram <143586258+Avanteesh@users.noreply.github.com>
2025-06-04 09:52:56 +0530

    version (1.0.5)

--- utilities/lint_module.py
+++ utilities/lint_module.py
@@ -7,7 +7,9 @@ from utilities.info import Config
 from collections import deque
 
 class ExtendedNodeVisitor(ast.NodeVisitor):
-    def __init__(self, ignore_warning: bool):
+    functions_defined = {}  # static variable showing functions that are defined and used n times!
+    def __init__(self, file_name: str, ignore_warning: bool):
+        self.file_name = file_name
         self.ignore_warning = ignore_warning
         self.globals = set()
 
@@ -22,25 +24,33 @@ class ExtendedNodeVisitor(ast.NodeVisitor):
             self.visit_ImportFrom(node)
         self.generic_visit(node)
 
-    def visit_FunctionDef(self, node):
+    def visit_FunctionDef(self, node: ast.AST):
+        if node.name not in ExtendedNodeVisitor.functions_defined:
+            ExtendedNodeVisitor.functions_defined[node.name] = {
+              "filename": self.file_name, "lineno": node.lineno, "used": False
+            }  
+            # a function that is defined may not have been used
         for arg_types in node.args.defaults:
             if isinstance(arg_types, ast.List) == True and self.ignore_warning == False:
                 warnings.warn(f"{col.Fore.RED}Warning at Line:{node.lineno}{col.Fore.WHITE} mutable default arguments are not allowed!")
         for child_nodes in node.body:
-            if isinstance(child_nodes, ast.Import) and self.ignore_warning == False:
+            if (isinstance(child_nodes, ast.Import) is True or isinstance(child_nodes, ast.ImportFrom) is True) and self.ignore_warning == False:
                 warnings.warn(f"{col.Fore.RED}Warning at Line:{col.Fore.WHITE} {child_nodes.lineno} Don't import locally inside a function!")
             elif isinstance(child_nodes, ast.FunctionDef):
                 self.visit_FunctionDef(child_nodes) # analyze lexical closures too!
     
-    def visit_Call(self, node):
+    def visit_Call(self, node: ast.AST):
         if node.func.id == "eval" and self.ignore_warning == False:
             warnings.warn(f"{col.Fore.RED}Warning at Line:{col.Fore.WHITE} {node.lineno} Do not use the 'eval'function, its prone to string injection attacks!")
+        if node.func.id in ExtendedNodeVisitor.functions_defined:
+            ExtendedNodeVisitor.functions_defined[node.func.id]["used"] = True  
+            # a user def function has been used!
 
-    def visit_ClassDef(self, node):
+    def visit_ClassDef(self, node: ast.AST):
         if (not (ord(node.name[0]) >= 65 and ord(node.name[0]) <= 91)) and self.ignore_warning == False:
             warnings.warn(f"{col.Fore.RED}Warning: {col.Fore.WHITE} class names must be capital!")
 
-    def visit_ImportFrom(self, node):
+    def visit_ImportFrom(self, node: ast.AST):
         # prevent wild card imports to avoid namespace collision!
         if node.names[0].name == "*" and self.ignore_warning == False:
             warnings.warn(f"{col.Fore.RED}Warning: {col.Fore.WHITE} wildcard import can lead to namespace pollution, instead import specific functions only!")
@@ -60,11 +70,14 @@ def analyzePySourceFiles(ignore_warning: bool=False):
                 tree = ""
                 with open(_file, "r") as f1:
                     tree = ast.parse(f1.read())
-                node_vistor = ExtendedNodeVisitor(ignore_warning)
+                node_vistor = ExtendedNodeVisitor(_file, ignore_warning)
                 node_vistor.visit(tree)
             elif os.path.isdir(content):
                 if first == "." and content == Config.DEPS_FOLDER.value:pass
                 else:queue.append(os.path.join(first, content))
+    for function_name, details in ExtendedNodeVisitor.functions_defined.items():
+        if details["used"] == False:  # if a user-defined function is not used, raise a warning!
+            warnings.warn(f"""{col.Fore.YELLOW}Warning: The function '{function_name}' has not been Used defined at: {details["filename"]} at line {details["lineno"]}{col.Fore.WHITE}""")
 
 
 
